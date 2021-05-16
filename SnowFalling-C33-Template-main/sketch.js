6af93ac5e6069a89ebe4347cbec650fb065a32e7
Spartan1005 <73547583+Spartan1005@users.noreply.github.com>
2021-05-16 12:22:48 -0400

    Add files via upload

---
+++ SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,19 @@
+
+function preload() {
+  bgImage("sprites/snow1.jpg")
+}
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+}
+
+  function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
